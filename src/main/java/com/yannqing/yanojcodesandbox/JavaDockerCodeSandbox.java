a73694c0addbda3d15cffec07d15a436e87c9c14
yannqing <67121851@qq.com>
2024-08-23 10:14:13 +0800

    change: complete the docker operate codeSandBox

--- src/main/java/com/yannqing/yanojcodesandbox/JavaDockerCodeSandbox.java
+++ src/main/java/com/yannqing/yanojcodesandbox/JavaDockerCodeSandbox.java
@@ -2,7 +2,18 @@ package com.yannqing.yanojcodesandbox;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.core.util.ArrayUtil;
 import cn.hutool.core.util.StrUtil;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerCmd;
+import com.github.dockerjava.api.command.ExecCreateCmdResponse;
+import com.github.dockerjava.api.model.*;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.core.command.ExecStartResultCallback;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
 import com.yannqing.yanojcodesandbox.model.ExecuteCodeRequest;
 import com.yannqing.yanojcodesandbox.model.ExecuteCodeResponse;
 import com.yannqing.yanojcodesandbox.model.ExecuteMessage;
@@ -54,6 +65,16 @@ public class JavaDockerCodeSandbox implements CodeSandBox {
     @Override
     public ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest) {
 
+
+        DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+        DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .build();
+        DockerClient dockerClient =  DockerClientImpl.getInstance(config, httpClient);
+
+
+
+
         List<String> inputList = executeCodeRequest.getInputList();
         String code = executeCodeRequest.getCode();
         String language = executeCodeRequest.getLanguage();
@@ -81,8 +102,75 @@ public class JavaDockerCodeSandbox implements CodeSandBox {
         }
 
         // 3. 执行代码，得到输出结果
-        String containerId = dockerService.createContainer("love", userCodeParentPath);
-        List<ExecuteMessage> executeMessageList = dockerService.execContainer(containerId, inputList);
+        // 创建容器配置
+        HostConfig hostConfig = new HostConfig();
+        hostConfig.withMemory(100 * 1000 * 1000L);      // 指定内存限制为 100M
+        hostConfig.withMemorySwap(0L);
+        hostConfig.withCpuCount(1L);                    // 指定分配的 CPU 为 1
+        // 设置挂载
+        hostConfig.setBinds(new Bind(userCodeParentPath, new Volume("/yannqing/yanojsandbox")));
+
+        // 构建创建容器命令
+        CreateContainerCmd createContainerCmd = dockerClient
+                .createContainerCmd("amazoncorretto:17-alpine-jdk")
+                .withHostConfig(hostConfig)
+                .withName("love")
+                .withNetworkDisabled(true)   // 禁止网络使用
+                .withReadonlyRootfs(true)
+                .withAttachStdin(true)
+                .withAttachStderr(true)
+                .withAttachStdout(true)
+                .withTty(true);             // 创建可交互的窗口
+
+        // 执行创建容器命令
+        String containerId = createContainerCmd.exec().getId();
+        dockerClient.startContainerCmd(containerId).exec();
+
+        // 执行命令
+        List<ExecuteMessage> executeMessageList = new ArrayList<>();
+        for (String inputArgs : inputList) {
+            String[] inputArgsArray = inputArgs.split(" ");
+            String[] cmdArray = ArrayUtil.append(new String[]{"java", "-cp", "/app", "Main"}, inputArgsArray);
+            ExecCreateCmdResponse execCreateCmdResponse = dockerClient.execCreateCmd(containerId)
+                    .withCmd(cmdArray)
+                    .withAttachStderr(true)
+                    .withAttachStdin(true)
+                    .withAttachStdout(true)
+                    .exec();
+            System.out.println("创建执行命令，" + execCreateCmdResponse);
+
+            ExecuteMessage executeMessage = new ExecuteMessage();
+            final String[] message = {null};
+            final String[] errorMessage = {null};
+
+            String execId =execCreateCmdResponse.getId();
+            ExecStartResultCallback execStartResultCallback =new ExecStartResultCallback() {
+                @Override
+                public void onNext(
+                        Frame frame) {
+                    StreamType streamType = frame.getStreamType();
+                    if (StreamType.STDERR.equals(streamType)) {
+                        errorMessage[0] = new String(frame.getPayload());
+                        System.out.println("输出错误结果：" + errorMessage[0]);
+                    } else {
+                        message[0] = new String(frame.getPayload());
+                        System.out.println("输出结果：" + message[0]);
+                    }
+                    super.onNext(frame);
+                }
+            };
+            try {
+                dockerClient.execStartCmd(execId)
+                        .exec(execStartResultCallback)
+                        .awaitCompletion();
+            } catch (InterruptedException e) {
+                System.out.println("程序执行异常");
+                throw new RuntimeException(e);
+            }
+            executeMessage.setMessage(message[0]);
+            executeMessage.setErrorMessage(errorMessage[0]);
+            executeMessageList.add(executeMessage);
+        }
 
         // 4、整理输出
         // 取用时最大值，便于判断是否超时
